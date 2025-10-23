4174094c72bb920c27f7b6ffcd796cd7a814f95c
santukumar01 <santu13102000@gmail.com>
2025-10-23 13:40:14 +0530

    otp on phone Number

--- backend/services/twillioPhoneNumber.js
+++ backend/services/twillioPhoneNumber.js
@@ -1,9 +1,55 @@
 const twilio = require("twilio");
 
-const twilio_token_sid = process.env.TWILLIO_TOKEN_SID;
+const serviceSid = process.env.TWILLIO_SERVICE_SID;
 const accountSid = process.env.TWILLIO_ACCOUNT_SID;
 const authToken = process.env.TWILLIO_AUTH_TOKEN;
 
 const client = twilio(accountSid, authToken);
 
-// 1 .22 .07
+// send otp to PhoneNumber
+
+const sendOtpToPhoneNumber = async (phoneNumber) => {
+  try {
+    console.log("Sending Otp to PhoneNumber", phoneNumber);
+    if (!phoneNumber) {
+      throw new Error("phone number is required");
+    }
+    const response = await client.verify.v2
+      .services(serviceSid)
+      .verifications.create({
+        to: phoneNumber,
+        channel: "sms",
+      });
+    console.log("this is my response", response);
+    return response;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to send Otp");
+  }
+};
+
+// verify the Otp
+
+const verifyOtp = async (phoneNumber, otp) => {
+  try {
+    console.log("this  is otp from service", otp);
+    console.log("on this PhoneNumber", phoneNumber);
+
+    const response = await client.verify.v2
+      .services(serviceSid)
+      .verificationChecks.create({
+        to: phoneNumber,
+        code: otp,
+      });
+    console.log("this is my response", response);
+    return response;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Otp verification failed");
+  }
+};
+
+module.exports = {
+  sendOtpToPhoneNumber,
+  verifyOtp,
+};
