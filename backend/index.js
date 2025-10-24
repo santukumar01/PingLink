9a288de6a759d6fe26f8ac77dc361b1a71215857
santukumar01 <santu13102000@gmail.com>
2025-10-24 11:49:19 +0530

    Otp verification

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,27 @@ const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
 const connectDb = require("./config/dbConnect");
+const authRoutes = require("../backend/routes/authRoutes");
 
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT;
-process.env.connectDb();
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// datbase conncetion
+connectDb();
+
+// Routes
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
